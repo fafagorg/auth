d7e1adb05023b285f6fb76a042245c6b2e0055c6
Roberto Hermoso <rhermoso98@gmail.com>
2020-12-17 20:28:45 +0100

    feat: lint-fix

--- connection.js
+++ connection.js
@@ -6,29 +6,28 @@ const {
   MONGO_DB
 } = process.env;
 
-
 const options = {
   useNewUrlParser: true,
   reconnectTries: Number.MAX_VALUE,
   reconnectInterval: 500,
-  connectTimeoutMS: 10000,
+  connectTimeoutMS: 10000
 };
 
 const url = `mongodb://172.17.0.1:${MONGO_PORT}/${MONGO_DB}`;
-console.log(url)
+console.log(url);
 const ms = 10000;
 
-const connect = ()=>{
-  mongoose.connect(url, options).then( function() {
+const connect = () => {
+  mongoose.connect(url, options).then(function () {
     console.log('MongoDB is connected');
   })
-    .catch( function(err) {
+    .catch(function (err) {
       setTimeout(() => {
-        connect()
+        connect();
       }, ms);
-      console.log("Failed connected to mongo, retrying in " + ms)
+      console.log('Failed connected to mongo, retrying in ' + ms);
       console.log(err);
-  });
-}
+    });
+};
 
-connect()
+connect();
